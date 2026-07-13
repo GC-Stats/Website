662b3ab0513dfe4b73ceeb4d8e0b488a3b5f7588
Lacy <lacy@gc-stats.app>
2026-07-13 20:01:10 +0000

    Added translation using Weblate (Portuguese)

---
+++ lang/pt/finance.php
@@ -0,0 +1,35 @@
+<?php
+return [
+    'title' => '',
+    'subtitle' => '',
+    'intro' => "",
+    'transparency_link' => '',
+    'summary' => [
+        'title' => '',
+        'income' => '',
+        'expense' => '',
+        'balance' => '',
+    ],
+    'current_year' => [
+        'title' => '',
+    ],
+    'average' => [
+        'title' => '',
+    ],
+    'table' => [
+        'date' => '',
+        'category' => '',
+        'label' => '',
+        'amount' => '',
+        'source' => '',
+        'view_source' => '',
+    ],
+    'type' => [
+        'income' => '',
+        'expense' => '',
+    ],
+    'empty' => '',
+    'show_older' => '',
+    'hide_older' => '',
+    'recent_period' => '',
+];
