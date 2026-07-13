6d1cc0c44b33d1aad1afd6efc9894088d24a58c9
Lacy <lacy@gc-stats.app>
2026-07-13 20:01:11 +0000

    Added translation using Weblate (Portuguese)

---
+++ lang/pt/help/add_tournament.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'title' => '',
+    'intro' => '',
+    'ticket' => [
+        'title' => '',
+        'body' => '',
+        'hint' => '',
+    ],
+    'info' => [
+        'title' => '',
+        'list' => [
+            'identity' => '',
+            'structure' => '',
+            'participants' => '',
+            'match_data' => '',
+        ],
+    ],
+    'footer_note' => '',
+];
