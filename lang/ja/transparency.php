ef45d71b08a58c0d973f0429e55f002bd39f8468
Lacy <lacy@gc-stats.app>
2026-07-13 20:01:16 +0000

    Added translation using Weblate (Japanese)

---
+++ lang/ja/transparency.php
@@ -0,0 +1,52 @@
+<?php
+return [
+    'title' => '',
+    'subtitle' => '',
+    'intro' => "",
+    'dev' => [
+        'title' => '',
+        'body' => '',
+        'link' => '',
+    ],
+    'hosting' => [
+        'title' => '',
+        'body' => '',
+        'providers' => [
+            'cdn' => [
+                'name' => '',
+                'role' => '',
+                'body' => '',
+            ],
+            'servers' => [
+                'name' => '',
+                'role' => '',
+                'body' => '',
+            ],
+            'mail' => [
+                'name' => '',
+                'role' => '',
+                'body' => '',
+            ],
+            'domain' => [
+                'name' => '',
+                'role' => '',
+                'body' => '',
+            ],
+        ],
+    ],
+    'data' => [
+        'title' => '',
+        'body' => "",
+        'link' => '',
+    ],
+    'finance' => [
+        'title' => '',
+        'body' => '',
+        'income' => '',
+        'expense' => '',
+        'balance' => '',
+        'link' => '',
+        'last_update' => '',
+        'no_entry' => '',
+    ],
+];
