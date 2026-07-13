ad7befbb2bb526c2e3cf2f25ce331bb6f150354c
Lacy <lacy@gc-stats.app>
2026-07-13 20:01:15 +0000

    Added translation using Weblate (Japanese)

---
+++ lang/ja/terms.php
@@ -0,0 +1,59 @@
+<?php
+return [
+    'title' => '',
+    'last_updated' => '',
+    'intro' => '',
+    'service' => [
+        'title' => '',
+        'text' => '',
+    ],
+    'access' => [
+        'title' => '',
+        'text' => '',
+    ],
+    'riot_data' => [
+        'title' => '',
+        'text' => '',
+        'items' => [
+            'riot_id' => '',
+            'matchs' => '',
+            'stats' => '',
+        ],
+        'opt_in' => '',
+        'correction' => '',
+    ],
+    'prohibited' => [
+        'title' => '',
+        'text' => '',
+        'items' => [
+            'gambling' => '',
+            'misinformation' => '',
+            'harassment' => '',
+            'illegal_activity' => '',
+            'scraping' => '',
+            'reselling' => '',
+        ],
+    ],
+    'api' => [
+        'title' => '',
+        'text' => '',
+    ],
+    'ip' => [
+        'title' => '',
+        'text' => '',
+    ],
+    'liability' => [
+        'title' => '',
+        'text' => '',
+    ],
+    'changes' => [
+        'title' => '',
+        'text' => '',
+    ],
+    'contact' => [
+        'title' => '',
+        'text' => '',
+        'email' => '',
+    ],
+    'riot_notice' => '',
+];
