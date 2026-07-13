4b2a8f9826c2748f0df664aede1585ab709bce03
Lacy <lacy@gc-stats.app>
2026-07-13 20:01:11 +0000

    Added translation using Weblate (Chinese (Simplified Han script))

---
+++ lang/zh_Hans/layout.php
@@ -0,0 +1,70 @@
+<?php
+return [
+    'searchbar' => '',
+    'searching' => '',
+    'search_results' => '',
+    'skip_to_content' => '',
+    'noresult' => '',
+    'recent_searches' => '',
+    'clear' => '',
+    'see_more' => '',
+    'nav' => [
+        'aria_label' => '',
+        'open_menu' => '',
+        'close_menu' => '',
+    ],
+    'lang' => [
+        'switcher_label' => '',
+        'dropdown_label' => '',
+    ],
+    'config' => [
+        'button_label' => '',
+        'panel_label' => '',
+        'title' => '',
+        'close' => '',
+        'theme' => [
+            'title' => '',
+            'dark' => '',
+            'white' => '',
+        ],
+        'accent' => [
+            'title' => '',
+            'none' => '',
+            'pride' => '',
+        ],
+        'timezone' => [
+            'title' => '',
+            'description' => '',
+        ],
+        'time_format' => [
+            'title' => '',
+            '12h' => '',
+            '24h' => '',
+        ],
+    ],
+    'type' => [
+        'players' => '',
+        'teams' => '',
+        'tournaments' => '',
+    ],
+    'footer' => [
+        'description' => '',
+        'copyright' => '',
+        'socials' => '',
+        'terms' => '',
+        'legal' => '',
+        'privacy' => '',
+        'about' => [
+            'title' => '',
+            'about_us' => '',
+            'dmca' => '',
+            'dev_doc' => '',
+            'transparency' => '',
+        ],
+        'informations' => [
+            'title' => '',
+            'update_page' => '',
+            'add_tournament' => '',
+        ],
+    ],
+];
