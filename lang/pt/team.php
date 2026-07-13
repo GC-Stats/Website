02af355306e04e3212961a23d889f5b6b1a4a05a
Lacy <lacy@gc-stats.app>
2026-07-13 20:01:14 +0000

    Added translation using Weblate (Portuguese)

---
+++ lang/pt/team.php
@@ -0,0 +1,37 @@
+<?php
+return [
+    'title' => [
+        'index' => '',
+        'history' => '',
+        'matches' => '',
+        'maps' => '',
+    ],
+    'players' => '',
+    'old_players' => '',
+    'news' => '',
+    'seemore' => '',
+    // Other pages text
+    'matches_history' => '',
+    'players_history' => '',
+    'empty' => [
+        'matches_history' => '',
+        'players_history' => '',
+        'maps' => '',
+    ],
+    'nav' => [
+        'aria_label' => '',
+        'overview' => '',
+        'matches' => '',
+        'maps' => '',
+        'players_history' => '',
+    ],
+    'maps' => [
+        'title' => '',
+        'record' => '',
+        'atk_wr' => '',
+        'def_wr' => '',
+        'pick_rate' => '',
+        'comps' => '',
+        'vs' => '',
+    ],
+];
