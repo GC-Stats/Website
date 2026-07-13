5661006ede139cc154ef2d18da0553fa5c678b98
Lacy <lacy@gc-stats.app>
2026-07-13 20:01:08 +0000

    Added translation using Weblate (Arabic)

---
+++ lang/ar/takedown.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    'title' => '',
+    'subtitle' => '',
+    'intro' => '',
+    'how_to' => [
+        'title' => '',
+        'text' => '',
+    ],
+    'info_needed' => [
+        'title' => '',
+        'item1' => '',
+        'item2' => '',
+        'item3' => '',
+    ],
+    'channels' => [
+        'discord' => '',
+        'email' => '',
+    ],
+    'footer' => '',
+];
