d776c821f71fc1f96d1061bedc9997b1979ef149
Lacy <lacy@gc-stats.app>
2026-07-13 20:01:09 +0000

    Added translation using Weblate (Spanish)

---
+++ lang/es/developers.php
@@ -0,0 +1,37 @@
+<?php
+return [
+    'title' => '',
+    'intro' => '',
+    'api_key' => [
+        'title' => '',
+        'body' => '',
+        'get_a_key' => '',
+        'warning' => '',
+        'step_1' => '',
+        'step_2' => '',
+        'step_3' => '',
+        'btn' => '',
+        'forbidden_title' => '',
+        'forbidden_text' => '',
+    ],
+    'swagger' => [
+        'title' => '',
+        'body' => '',
+        'btn' => '',
+    ],
+    'dashboard' => [
+        'title' => '',
+        'body' => '',
+        'btn' => '',
+    ],
+    'opendata' => [
+        'title' => '',
+        'body' => '',
+        'btn' => '',
+    ],
+    'git' => [
+        'title' => '',
+        'body' => '',
+        'badge' => '',
+    ],
+];
