a7c87c963a7eec7604b88ff7df731d731d76dd43
Lacy <lacy@gc-stats.app>
2026-07-13 20:01:14 +0000

    Added translation using Weblate (Italian)

---
+++ lang/it/privacy.php
@@ -0,0 +1,43 @@
+<?php
+return [
+    'title' => '',
+    'last_updated' => '',
+    'intro' => '',
+    'analytics' => [
+        'title' => '',
+        'text' => '',
+    ],
+    'public_data' => [
+        'title' => '',
+        'text' => '',
+    ],
+    'private_data' => [
+        'title' => '',
+        'text' => '',
+        'discord_usage' => '',
+        'riot_usage' => '',
+    ],
+    'opt_in' => [
+        'title' => '',
+        'text' => '',
+    ],
+    'data_structure' => [
+        'title' => '',
+        'text' => '',
+        'button' => '',
+    ],
+    'retention' => [
+        'title' => '',
+        'text' => '',
+    ],
+    'rights' => [
+        'title' => '',
+        'text' => '',
+        'contact' => '',
+    ],
+    'cookies' => [
+        'title' => '',
+        'text' => '',
+    ],
+    'takedown' => '',
+];
