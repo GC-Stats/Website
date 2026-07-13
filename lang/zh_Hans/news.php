4db5a31cd0e916f5c6aceb21103edd5178655aa4
Lacy <lacy@gc-stats.app>
2026-07-13 20:01:13 +0000

    Added translation using Weblate (Chinese (Simplified Han script))

---
+++ lang/zh_Hans/news.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'featured' => '',
+    'articles' => '',
+    'no_articles' => '',
+    'press_section' => '',
+    'locale_info_label' => '',
+    'locale_info' => '',
+    'related' => '',
+    'author' => '',
+    'publisher' => '',
+];
