a136a65e3d4f824d4d24d20f8fa7a4e8bea5f48d
Lars Schmidt <lars.h.schmidt20@gmail.com>
2026-07-28 13:40:06 +0000

    chore(l10n): update German translation
    Translation: Help - Add Tournament
    Language: German
    Progress: 90.9% (10 of 11 strings)
    
    Translate-URL: http://translate.gc-stats.app/projects/website/help-add-tournament/de/
    Translator: Lars Schmidt <lars.h.schmidt20@gmail.com>

--- lang/de/help/add_tournament.php
+++ lang/de/help/add_tournament.php
@@ -1,20 +1,20 @@
 <?php
 return [
-    'title' => '',
-    'intro' => '',
+    'title' => 'Turnier hinzufügen',
+    'intro' => 'Organisieren Sie ein Game Changers Turnier? Wir können es auf unserer Plattform integrieren.',
     'ticket' => [
-        'title' => '',
-        'body' => '',
-        'hint' => '',
+        'title' => 'Ticket eröffnen',
+        'body' => 'Jede Turnierhinzufügung erfordert eine manuelle Validierung. Treten Sie unserem Discord bei und erstellen Sie ein Ticket in dem spezifischen Kanal für Organisierer.',
+        'hint' => 'Notiz: Erlauben Sie es für 48 bis 72 Stunden vor Turnierstart für optimale Integrierung.',
     ],
     'info' => [
-        'title' => '',
+        'title' => 'benötigte Informationen',
         'list' => [
-            'identity' => '',
-            'structure' => '',
-            'participants' => '',
-            'match_data' => '',
+            'identity' => '<strong>Identität:</strong> Turniername, Datum, Preisgeld und grundlegende Informationen.',
+            'structure' => '<strong>Struktur:</strong> Liste der phasen (Playoffs, Gruppenphase, Round Robin).',
+            'participants' => '<strong>Teilnehmer:</strong> vollständige Liste aller teilnehmenden Teams und Spielerinnen.',
+            'match_data' => '<strong>Spieldaten:</strong> Liste der Spiele, Ergebnisse, Map-Veto (gewählt/gebannt) und Statistiken, falls verfügbar.',
         ],
     ],
-    'footer_note' => '',
+    'footer_note' => 'Die GC Stats Mitarbeiter werden Sie über die Dauer ihres Turniers unterstützen.',
 ];
